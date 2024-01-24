3d44ed7237949fc15d6e20ec008bb84a10330080
LNJAMBI001 <LNjambi001@gmail.com>
2024-01-24 22:42:07 +0300

    First attempt at deploying

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 # app.py
 import streamlit as st
 import joblib
+import xgboost as xgb
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Load the model and vectorizer
-model = joblib.load(xgb_model'best_model.pkl')
+model = joblib.load('best_model.pkl')
 vectorizer = joblib.load('vectorizer.pkl')
+#streamlit app
+banner_image_path = r'C:/Users/ADMIN/Pictures/Eabl_download.png'  
+st.image(banner_image_path, use_column_width=True)
 
-# Streamlit app
 def main():
-    st.title('Sentiment Analysis App')
+    
+    st.title('EABL Sentiment Analysis App')
     text = st.text_area('Enter text for sentiment analysis:', '')
 
     if st.button('Analyze'):
@@ -19,11 +24,14 @@ def predict_sentiment(text):
     # Preprocess the input text using the loaded vectorizer
     text_vectorized = vectorizer.transform([text])
 
+    # Ensure the input data is in the appropriate format for prediction
+    dtest = xgb.DMatrix(text_vectorized)
+
     # Use the loaded model to predict sentiment
-    sentiment = model.predict(text_vectorized)[0]
+    sentiment = model.predict(dtest)[0]
 
     # Adjust this based on your specific model and preprocessing steps
-    return 'Positive' if sentiment == 1 else 'Negative'
+    return 'Negative' if sentiment == 1 else 'Positive'
 
 if __name__ == '__main__':
     main()
